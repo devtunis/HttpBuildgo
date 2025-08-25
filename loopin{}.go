c678a70b4d6508e32d94b6e97b19b34302607605
KernelNest <121437369+devtunis@users.noreply.github.com>
2025-08-25 12:52:10 +0100

    Create loopin{}.go

---
+++ loopin{}.go
@@ -0,0 +1,26 @@
+// Online Go compiler to run Golang program online
+// Print "Try programiz.pro" message
+
+package main
+import "fmt"
+
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
+ 
+
+func main() {
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+for _ ,num :=range albums {
+    fmt.Println(num)
+}
+
+}
